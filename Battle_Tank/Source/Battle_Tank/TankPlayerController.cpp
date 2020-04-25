47a4ec8e55a87fb69ab6332d331b059e9f64a44c
DESKTOP-8TISN47\mmuro <mmuro010@gmail.com>
2020-04-24 19:33:23 -0700

    203 Delegating to components

--- Battle_Tank/Source/Battle_Tank/TankPlayerController.cpp
+++ Battle_Tank/Source/Battle_Tank/TankPlayerController.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "TankPlayerController.h"
 
-/*ATank* ATankPlayerController::ATank* GetControlledTank() const;
+ATank*ATankPlayerController::GetConrolledTank() const
 {
-	Cast<Atank>(GetPawn());
+	return Cast<ATank>(GetPawn());
 }
--- Battle_Tank/Source/Battle_Tank/TankPlayerController.h
+++ Battle_Tank/Source/Battle_Tank/TankPlayerController.h
@@ -3,18 +3,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
-//#include "Tank.h"
+#include "Tank.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
 
-
- 
+/**
+ * 
+ */
 UCLASS()
 class BATTLE_TANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-/**
- * 	
+	
 public:
-	ATank* GetControlledTank() const;*/
+	ATank*GetConrolledTank() const;
 };
