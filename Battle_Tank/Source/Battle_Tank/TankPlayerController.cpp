818ec02a965f485a967aeb10f4d29233baeb4d03
DESKTOP-8TISN47\mmuro <mmuro010@gmail.com>
2020-04-26 18:01:14 -0700

    Creating an Out Paramerter Mechod

--- Battle_Tank/Source/Battle_Tank/TankPlayerController.cpp
+++ Battle_Tank/Source/Battle_Tank/TankPlayerController.cpp
@@ -3,6 +3,11 @@
 
 #include "TankPlayerController.h"
 
+//tick
+	//super
+	//AimTowardsCrosshair();
+
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -22,4 +27,32 @@ void ATankPlayerController::BeginPlay()
 ATank*ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerController ticking away"));
+
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; //out parameter
+	if(GetSightRayHitLocation(HitLocation))
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		//TODO tell conrolled tank to aim at this point.
+	}
+}
+
+//Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);
+	return true;
 }
--- Battle_Tank/Source/Battle_Tank/TankPlayerController.h
+++ Battle_Tank/Source/Battle_Tank/TankPlayerController.h
@@ -14,9 +14,18 @@ UCLASS()
 class BATTLE_TANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
 public:
 	ATank*GetControlledTank() const;
-	
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	void BeginPlay() override;
+
+private:
+	//start barrel so that a shot would it where crosshair intersects the world
+	void AimTowardsCrosshair();
+	//return an out paramerter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 };
