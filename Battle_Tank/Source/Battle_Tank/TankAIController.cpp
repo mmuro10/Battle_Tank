6a37fbb9e8b7e9af865e0b4ec764db09ef3d93be
DESKTOP-8TISN47\mmuro <mmuro010@gmail.com>
2020-04-26 17:19:06 -0700

    lec 208 Get Player Controller w/ C++

---
+++ Battle_Tank/Source/Battle_Tank/TankAIController.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController can not find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found Player: %s"), *(PlayerTank->GetName()));
+	}
+
+
+}
+ATank*ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	//if(!PlayerPawn) { return PlayerPawn; }
+	if (!PlayerPawn) {return nullptr;}
+	return Cast<ATank>(PlayerPawn);
+}
---
+++ Battle_Tank/Source/Battle_Tank/TankAIController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "Engine/World.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+private:
+	ATank*GetControlledTank() const;
+	ATank*GetPlayerTank() const;
+
+	void BeginPlay() override;
+};
